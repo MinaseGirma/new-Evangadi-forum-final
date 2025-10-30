583ea2e6304b97c692f8409c28df3e5a48b48381
Minase Girma <109416852+MinaseGirma@users.noreply.github.com>
2025-10-30 18:10:58 +0100

    Add new CORS origin for evangadiforummg.netlify.app

--- Server/app.js
+++ Server/app.js
@@ -12,6 +12,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://2025-evangadi-forum-project.netlify.app",
+      "https://evangadiforummg.netlify.app/"
     ],
     credentials: true,
   })
